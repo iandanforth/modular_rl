021d428d2466155a6685ee01ca728ecf397d41e6
idanforth <iandanforth@gmail.com>
2016-09-25 14:32:36 -0700

    Start PEP8 on run_pg.py; Remove broken pylintrc

--- run_pg.py
+++ run_pg.py
@@ -1,67 +1,121 @@
 #!/usr/bin/env python
+
 """
 This script runs a policy gradient algorithm
 """
 
+# Standard Library
+import argparse
+import sys
+import cPickle
+import shutil
+import os
+import logging
 
-from gym.envs import make
-from modular_rl import *
-import argparse, sys, cPickle
-from tabulate import tabulate
-import shutil, os, logging
+# Third Party
 import gym
+import numpy as np
+from gym.envs import make
+from tabulate import tabulate
+
+
+# Local
+#from modular_rl import *
+from modular_rl.core import (VIDEO_NEVER,
+                             get_agent_cls,
+                             animate_rollout,
+                             run_policy_gradient_algorithm)
+from modular_rl.misc_utils import (update_argument_parser,
+                                   prepare_h5_file,
+                                   GENERAL_OPTIONS)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    update_argument_parser(parser, GENERAL_OPTIONS)    
-    parser.add_argument("--env",required=True)
-    parser.add_argument("--agent",required=True)
-    parser.add_argument("--plot",action="store_true")
-    args,_ = parser.parse_known_args([arg for arg in sys.argv[1:] if arg not in ('-h', '--help')])
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    update_argument_parser(parser, GENERAL_OPTIONS)
+
+    parser.add_argument("--env", required=True)
+    parser.add_argument("--agent", required=True)
+    parser.add_argument("--plot", action="store_true")
+
+    args, _ = parser.parse_known_args(
+        [arg for arg in sys.argv[1:] if arg not in ('-h', '--help')]
+    )
+
     env = make(args.env)
+
     env_spec = env.spec
+
     mondir = args.outfile + ".dir"
-    if os.path.exists(mondir): shutil.rmtree(mondir)
+
+    if os.path.exists(mondir):
+        shutil.rmtree(mondir)
+
     os.mkdir(mondir)
-    env.monitor.start(mondir, video_callable=None if args.video else VIDEO_NEVER)
+
+    video_callable = None
+    if not args.video:
+        video_callable = VIDEO_NEVER
+    env.monitor.start(mondir, video_callable=video_callable)
+
     agent_ctor = get_agent_cls(args.agent)
+
     update_argument_parser(parser, agent_ctor.options)
+
     args = parser.parse_args()
-    if args.timestep_limit == 0: 
-        args.timestep_limit = env_spec.timestep_limit    
+
+    if args.timestep_limit == 0:
+        args.timestep_limit = env_spec.timestep_limit
+
     cfg = args.__dict__
+
     np.random.seed(args.seed)
+
     agent = agent_ctor(env.observation_space, env.action_space, cfg)
+
     if args.use_hdf:
         hdf, diagnostics = prepare_h5_file(args)
+
     gym.logger.setLevel(logging.WARN)
 
     COUNTER = 0
+
     def callback(stats):
         global COUNTER
-        COUNTER += 1  
+        COUNTER += 1
         # Print stats
         print "*********** Iteration %i ****************" % COUNTER
-        print tabulate(filter(lambda (k,v) : np.asarray(v).size==1, stats.items())) #pylint: disable=W0110
+        print tabulate(
+            filter(lambda (k,v) : np.asarray(v).size == 1, stats.items())
+        )
         # Store to hdf5
         if args.use_hdf:
-            for (stat,val) in stats.items():
-                if np.asarray(val).ndim==0:
+            for (stat, val) in stats.items():
+                if np.asarray(val).ndim == 0:
                     diagnostics[stat].append(val)
                 else:
                     assert val.ndim == 1
                     diagnostics[stat].extend(val)
-            if args.snapshot_every and ((COUNTER % args.snapshot_every==0) or (COUNTER==args.n_iter)): 
-                hdf['/agent_snapshots/%0.4i'%COUNTER] = np.array(cPickle.dumps(agent,-1))
+            snapshot_time = (
+                (COUNTER % args.snapshot_every == 0) or (COUNTER == args.n_iter)
+            )
+            if args.snapshot_every and snapshot_time:
+                hdf['/agent_snapshots/%0.4i' % COUNTER] = \
+                np.array(cPickle.dumps(agent, -1))
         # Plot
         if args.plot:
             animate_rollout(env, agent, min(500, args.timestep_limit))
 
-    run_policy_gradient_algorithm(env, agent, callback=callback, usercfg = cfg)
+    run_policy_gradient_algorithm(env, agent, callback=callback, usercfg=cfg)
 
     if args.use_hdf:
         hdf['env_id'] = env_spec.id
-        try: hdf['env'] = np.array(cPickle.dumps(env, -1))
-        except Exception: print "failed to pickle env" #pylint: disable=W0703
-    
+        try:
+            hdf['env'] = np.array(cPickle.dumps(env, -1))
+        except Exception:
+            print "failed to pickle env"
+
     env.monitor.close()
